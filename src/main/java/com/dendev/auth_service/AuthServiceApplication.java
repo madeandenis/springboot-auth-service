441521db3ce184d4770037740a3db903f3903927
madeandenis <123639454+madeandenis@users.noreply.github.com>
2024-05-27 23:18:17 +0300

    Configure db connections through .env

---
+++ src/main/java/com/dendev/auth_service/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.dendev.auth_service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
