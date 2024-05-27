58a2283284a00e3860b01f52ec0d283b960a0fdc
madeandenis <123639454+madeandenis@users.noreply.github.com>
2024-05-27 23:42:50 +0300

    Add repos

--- src/main/java/com/dendev/auth_service/repository/RoleRepository.java
+++ src/main/java/com/dendev/auth_service/repository/RoleRepository.java
@@ -1,2 +1,11 @@
-package com.dendev.auth_service.repository;public class RoleRepository {
+package com.dendev.auth_service.repository;
+
+import com.dendev.auth_service.model.ERole;
+import com.dendev.auth_service.model.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
 }
--- src/main/java/com/dendev/auth_service/repository/UserRepository.java
+++ src/main/java/com/dendev/auth_service/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.dendev.auth_service.repository;public class UserRepository {
+package com.dendev.auth_service.repository;
+
+import com.dendev.auth_service.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
